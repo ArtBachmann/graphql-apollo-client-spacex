704e715d75131bb65ea08b4e980ae89e63bebbf1
Art Bachmann <bachmann.art@gmail.com>
2019-12-23 19:42:38 +0200

    commit before deployment

---
+++ public/precache-manifest.40f6dd48dc859e56f34bbd2bb97d455e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1bc06d55514def2766b55a776e55a29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37eb066360b24b3e4c6c",
+    "url": "/static/css/2.debaa233.chunk.css"
+  },
+  {
+    "revision": "c4696dfdacc84d7b0857",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "37eb066360b24b3e4c6c",
+    "url": "/static/js/2.1e5d8041.chunk.js"
+  },
+  {
+    "revision": "c4696dfdacc84d7b0857",
+    "url": "/static/js/main.53835346.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
